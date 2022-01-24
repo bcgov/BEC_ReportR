281d3adfc893c964f00b2915be837fee15c5f201
Oamar Kanji <oamarkanji@gmail.com>
2022-01-24 11:41:26 -0800

    second commit

--- _functions/_CreateHierReportData.R
+++ _functions/_CreateHierReportData.R
@@ -2,7 +2,7 @@ createHierReportData <- function(level_name, level_value, SUTab) {
   
   
   # Select relevent rows from Heir.data
-  level_rows <- Hier.data %>% filter(!!as.name(level) == level_value )
+  level_rows <- Hier.data %>% filter(!!as.name(level_name) == level_value )
   
   # Levels
   site_units <- unique(level_rows$SiteUnit)
@@ -30,12 +30,28 @@ createHierReportData <- function(level_name, level_value, SUTab) {
   
   
   # Maybe return plot_numbers_in_level (or write it) and do below in a different step
-  
+ 
+   
   # Look up plots
-  heir_summary[[paste("plot_numbers_in_", level_name, sep="")]] <- list(SUTab$PlotNumber[SUTab$SiteUnit %in% site_units]) 
-  saveRDS(heir_summary, file=paste("./reports/", level_name, "/", str_replace_all(level_value, " ", "_"), ".RDS", sep = ""))
-  
+  # heir_summary[[paste("plot_numbers_in_", level_name, sep="")]] <- list(SUTab$PlotNumber[SUTab$SiteUnit %in% site_units]) 
+ 
   # Save heir report
-  #list.save(heir_summary, cat("./reports/", level_name, "/", str_replace_all(level_value, " ", "_"), ".rdata", sep = ""))
+  reports_dir <- file.path("reports", level_name) 
+  if (!dir.exists(reports_dir)) dir.create(reports_dir, recursive = TRUE)
+  
+  file_name <- paste(str_replace_all(level_value, " ", "_"), ".RDS",  sep="")
+  saveRDS(heir_summary, file = file.path(reports_dir, file_name))
+
+  plot_numbers_dir <- file.path("plotNumbers", level_name) 
+  if (!dir.exists(plot_numbers_dir)) dir.create(plot_numbers_dir, recursive = TRUE)
+  
+  plot_numbers_in_level <- list(SUTab$PlotNumber[SUTab$SiteUnit %in% site_units]) 
+  saveRDS(plot_numbers_in_level, file =(paste(plot_numbers_dir, "/", "plot_numbers_in_" , str_replace_all(level_value, " ", "_"), ".RDS", sep=""))) 
+  
+  # Save plot numbers 
+  # if (!file.exists(paste(dir, "/", "plot_numbers_in_", level_value, ".RDS", sep=""))) {
+  #   plot_numbers_in_level <- list(SUTab$PlotNumber[SUTab$SiteUnit %in% site_units]) 
+  #   saveRDS(plot_numbers_in_level, file =(paste("plot_numbers_in_" , level_value, ".RDS", sep=""))) 
+  # }  
   
 }
